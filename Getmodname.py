606a0429f0de02b8b76ec2156235977758bee615
TheBossMagnus <TheBossMagnus@proton.me>
2023-10-19 22:12:33 +0200

    Make Api request 10x time faster
    The power of async

--- Getmodname.py
+++ Getmodname.py
@@ -1,17 +1,21 @@
-def GetModName(ModId):
+async def GetModName(ModId):
     import requests
+    import asyncio
+    import aiohttp
+
     BaseUrl = "https://api.modrinth.com/v2/project"
     Url = BaseUrl + "/" + ModId
 
     try:
-        response = requests.get(Url)
-        response.raise_for_status()
-        responseJson = response.json()
-        return responseJson["title"]
-    except requests.exceptions.HTTPError as err:
-        print(f"Error: Could not get project info for id {ModId} from Modrinth API. Response code: {err.response.status_code}")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(Url) as response:
+                response.raise_for_status()
+                responseJson = await response.json()
+                return responseJson["title"]
+    except aiohttp.ClientResponseError as err:
+        print(f"Error: Could not get project info for id {ModId} from Modrinth API. Response code: {err.status}")
         print("Retry later, if the error persists open an issue on the GitHub repo.")
-    except requests.exceptions.ConnectionError as err:
+    except aiohttp.ClientConnectionError as err:
         print(f"Unable to connect do modrinth api,({err}). Check your internet connection and if Modrinth is online.")
-    except requests.exceptions.RequestException as err:
+    except aiohttp.ClientError as err:
         print(f"Error: {err}")
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import json
 import re
 import sys
+import asyncio
 from Getmodname import GetModName
 
 # Get the paths to old.json and new.json from command line arguments
@@ -8,78 +9,80 @@ if len(sys.argv) < 3:
     print("Usage: python Main.py <path_to_old_json> <path_to_new_json>")
     sys.exit(1)
 
-old_json_path = sys.argv[1]
-new_json_path = sys.argv[2]
+oldJsonPath = sys.argv[1]
+newJsonPath = sys.argv[2]
 
 # Load the old and new packs
-with open(old_json_path, 'r', encoding="utf-8") as f:
-    old_pack = json.load(f)
-with open(new_json_path, 'r', encoding="utf-8") as f:
-    new_pack = json.load(f)
+with open(oldJsonPath, 'r', encoding="utf-8") as f:
+    oldPack = json.load(f)
+with open(newJsonPath, 'r', encoding="utf-8") as f:
+    newPack = json.load(f)
 
 # Get the Minecraft version from both packs
-old_pack_mc_version = old_pack['dependencies']['minecraft']
-new_pack_mc_version = new_pack['dependencies']['minecraft']
+oldPackMcVersion = oldPack['dependencies']['minecraft']
+newPackMcVersion = newPack['dependencies']['minecraft']
 
 # Get the loader and his version from the index.json
-old_pack_loader = list(old_pack['dependencies'].keys())[0]
-old_pack_loader_version = old_pack['dependencies'][old_pack_loader]
-new_pack_loader = list(new_pack['dependencies'].keys())[0]
-new_pack_loader_version = new_pack['dependencies'][new_pack_loader]
+oldPackLoader = list(oldPack['dependencies'].keys())[0]
+oldPackLoaderVersion = oldPack['dependencies'][oldPackLoader]
+newPackLoader = list(newPack['dependencies'].keys())[0]
+newPackLoaderVersion = newPack['dependencies'][newPackLoader]
 
 
 # Load the list of mods from both packs
 # Foreach Files entry in the json get the path key value
-old_pack_urls = []
-for file in old_pack['files']:
-    old_pack_urls.append(file['downloads'])
+oldPackUrls = []
+for file in oldPack['files']:
+    oldPackUrls.append(file['downloads'])
 
-new_pack_urls = []
-for file in new_pack['files']:
-    new_pack_urls.append(file['downloads'])
-
-for NewMod in new_pack_urls.copy():
-    for OldMod in old_pack_urls.copy():
-        if NewMod == OldMod:
-            old_pack_urls.remove(OldMod)
-            new_pack_urls.remove(NewMod)
+newPackUrls = []
+for file in newPack['files']:
+    newPackUrls.append(file['downloads'])
 
 
+# If an url is in both packs its untached, so ignore it
+for newMod in newPackUrls.copy():
+    for oldMod in oldPackUrls.copy():
+        if newMod == oldMod:
+            oldPackUrls.remove(oldMod)
+            newPackUrls.remove(newMod)
 
-new_pack_ids = []
-for url in new_pack_urls:
-    new_pack_ids.append(re.search(r"(?<=data\/)[a-zA-Z0-9]{8}", str(url)).group(0))
 
-old_pack_ids = []
-for url in old_pack_urls:
-    old_pack_ids.append(re.search(r"(?<=data\/)[a-zA-Z0-9]{8}", str(url)).group(0))
 
-Updated_ids = []
-New_ids = []
-Removed_ids = []
+newPackIds = []
+for url in newPackUrls:
+    newPackIds.append(re.search(r"(?<=data\/)[a-zA-Z0-9]{8}", str(url)).group(0))
 
-for NewId in new_pack_ids.copy():
-    for OldId in old_pack_ids.copy():
-        if NewId == OldId:
-            old_pack_ids.remove(OldId)
-            new_pack_ids.remove(NewId)
-            Updated_ids.append(NewId)
+oldPackIds = []
+for url in oldPackUrls:
+    oldPackIds.append(re.search(r"(?<=data\/)[a-zA-Z0-9]{8}", str(url)).group(0))
 
-New_ids = new_pack_urls
-Removed_ids = old_pack_urls
-print(Removed_ids)
+updatedIds = []
+newIds = []
+removedIds = []
 
-if len(Updated_ids) > 3:
-    print("Updated mod:")
-    for ModId in Updated_ids:
-        print("- " + GetModName(ModId))
+# If an id is in both packs its updated, add it to the updated list
+for newId in newPackIds.copy():
+    for oldId in oldPackIds.copy():
+        if newId == oldId:
+            oldPackIds.remove(oldId)
+            newPackIds.remove(newId)
+            updatedIds.append(newId)
 
-if len(New_ids) > 3:
-    print("New mod:")
-    for ModId in New_ids:
-        print("- " + GetModName(ModId))
+newIds = newPackIds
+removedIds = oldPackIds
 
-if len(Removed_ids) > 3:
-    print("Removed mod:")
-    for ModId in Removed_ids:
-        print("- " + GetModName(ModId))
+async def main():
+    if updatedIds:
+        print("Updated mod:")
+        print("\n".join(["- " + name for name in await asyncio.gather(*[GetModName(ModId) for ModId in updatedIds])]))
+
+    if newIds:
+        print("New mod:")
+        print("\n".join(["- " + name for name in await asyncio.gather(*[GetModName(ModId) for ModId in newIds])]))
+
+    if removedIds:
+        print("Removed mod:")
+        print("\n".join(["- " + name for name in await asyncio.gather(*[GetModName(ModId) for ModId in removedIds])]))
+
+asyncio.run(main())
