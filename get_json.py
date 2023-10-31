21799a9e05508d4893cb37ca8219fcc3be69215a
TheBossMagnus <TheBossMagnus@proton.me>
2023-10-31 11:43:59 +0100

    fix json import

--- get_json.py
+++ get_json.py
@@ -1,4 +1,6 @@
-def get_json (json_path):
+import json
+
+def get_json(json_path):
     # Load the old and new packs
     with open(json_path, 'r', encoding="utf-8") as f:
-        return json_path.json.load(f)
+        return json.load(f)
