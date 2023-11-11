59367dc2589af81fef9f55aac19bfefe6c30a3a5
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-11 11:04:15 +0100

    make aiohttp import lazy and smaller

--- get_mod_name.py
+++ get_mod_name.py
@@ -1,24 +1,23 @@
-import aiohttp
-
 async def get_mod_name(mod_id):
+    from aiohttp import ClientSession, ClientResponseError, ClientConnectionError, ClientError
     base_url = "https://api.modrinth.com/v2/project"
     headers = {'User-Agent':"TheBossMagnus/Mrpack_changelogger (thebossmagnus@proton.me)"}
     url = base_url + "/" + mod_id
 
     try:
         #Get mod name
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession() as session:
             async with session.get(url, headers=headers) as response:
                 response.raise_for_status()
                 response_json = await response.json()
                 return response_json["title"]
     #ID not existing or similar
-    except aiohttp.ClientResponseError as err:
+    except ClientResponseError as err:
         print(f"WARNING: Could not get project info for id {mod_id} from Modrinth API. Response code: {err.status}")
         print("Retry later, if the error persists open an issue on the GitHub repo.")
     #Connection error
-    except aiohttp.ClientConnectionError as err:
+    except ClientConnectionError as err:
         print(f"WARNING: Unable to connect to modrinth api, ({err}). Check your internet connection and if Modrinth is online.")
     #Generic error
-    except aiohttp.ClientError as err:
+    except ClientError as err:
         print(f"ERROR: {err}")
