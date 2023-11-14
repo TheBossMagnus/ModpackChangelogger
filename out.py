3079331578de45ea1d1ace5b870bde6a7bc8de6f
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-14 22:37:42 +0100

    Refactor markdown_out

--- out.py
+++ out.py
@@ -1,26 +1,22 @@
 import sys
 
+def format_changes(added, removed, updated, style):
+    actions = [("Added", added), ("Removed", removed), ("Updated", updated)]
+    
+    if style == 'bullet':
+        return "\n".join([f"## {title}\n" + "\n".join(f"- {mod}" for mod in mods) for title, mods in actions if mods])
+    elif style == 'comma':
+        return "\n".join([f"* **{title}**: " + ", ".join(mod for mod in mods) for title, mods in actions if mods])
+    elif style == 'ind_bullet':
+        return "Changes:\n" + "\n".join([f"* {action} {mod}" for action, mods in actions for mod in mods])
+    elif style == 'ind_comma':
+        return "**Changes:** " + ", ".join([f"{action} {mod}" for action, mods in actions for mod in mods])
+    else:
+        raise ValueError(f"Invalid style: {style}")
+
 def markdown_out(added, removed, updated, config):
-    def format_changes(changes, prefix, separator):
-        return prefix + separator.join(changes)
-
-    style_config = {
-        "comma": {"prefixes": {"added": "* Added: ", "removed": "\n* Removed: ", "updated": "\n* Updated: "}, "separator": ", "},
-        "bullet": {"prefixes": {"added": "## Added\n- ", "removed": "## Removed\n- ", "updated": "## Updated\n- "}, "separator": "\n- "},
-        "ind_bullet": {"prefixes": {"added": "\n* added ", "removed": "\n* removed ", "updated": "\n* updated "}, "separator": "\n* "},
-        "ind_comma": {"prefixes": {"added": "added ", "removed": "removed ", "updated": "updated "}, "separator": ", "}
-    }
-
     style = config['format']['style']
-    style_config = style_config.get(style, style_config["bullet"])
-
-    markdown_text = "Changes: " if "ind" in style else ""
-    if added:
-        markdown_text += format_changes(added, style_config["prefixes"]["added"], style_config["separator"]) + "\n"
-    if removed:
-        markdown_text += format_changes(removed, style_config["prefixes"]["removed"], style_config["separator"]) + "\n"
-    if updated:
-        markdown_text += format_changes(updated, style_config["prefixes"]["updated"], style_config["separator"]) + "\n"
+    markdown_text = format_changes(added, removed, updated, style)
 
     try:
         with open(config['output']['file_path'] + config['output']['file_name'], "w", encoding="utf-8") as f:
