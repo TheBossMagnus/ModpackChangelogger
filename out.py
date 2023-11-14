b0a71182d76bc8a273a3f43a6015b20916319f8c
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-14 22:06:51 +0100

    refactor out.py, needs further works

--- out.py
+++ out.py
@@ -1,100 +1,33 @@
 import sys
+
 def markdown_out(added, removed, updated, config):
+    def format_changes(changes, prefix, separator):
+        return prefix + separator.join(changes)
 
-    if config['format']['style'] == "comma":
-        markdown_text = comma_list(added, removed, updated)
-    elif config['format']['style']  == "bullet":
-        markdown_text = bullet_list(added, removed, updated)
-    elif config['format']['style'] == "ind_bullet":
-        markdown_text = ind_bullet_list(added, removed, updated)
-    elif config['format']['style'] == "ind_comma":
-        markdown_text = comma_list(added, removed, updated)
-    else:
-        markdown_text = bullet_list(added, removed, updated)
+    style_config = {
+        "comma": {"prefixes": {"added": "* Added: ", "removed": "\n* Removed: ", "updated": "\n* Updated: "}, "separator": ", "},
+        "bullet": {"prefixes": {"added": "## Added\n- ", "removed": "## Removed\n- ", "updated": "## Updated\n- "}, "separator": "\n- "},
+        "ind_bullet": {"prefixes": {"added": "\n* added ", "removed": "\n* removed ", "updated": "\n* updated "}, "separator": "\n* "},
+        "ind_comma": {"prefixes": {"added": "added ", "removed": "removed ", "updated": "updated "}, "separator": ", "}
+    }
 
-    # Write the Markdown text to the output file
+    style = config['format']['style']
+    style_config = style_config.get(style, style_config["bullet"])
+
+    markdown_text = "Changes: " if "ind" in style else ""
+    if added:
+        markdown_text += format_changes(added, style_config["prefixes"]["added"], style_config["separator"]) + "\n"
+    if removed:
+        markdown_text += format_changes(removed, style_config["prefixes"]["removed"], style_config["separator"]) + "\n"
+    if updated:
+        markdown_text += format_changes(updated, style_config["prefixes"]["updated"], style_config["separator"]) + "\n"
+
     try:
         with open(config['output']['file_path'] + config['output']['file_name'], "w", encoding="utf-8") as f:
-                    f.write(markdown_text)
-                    f.close()
+            f.write(markdown_text)
     except FileNotFoundError:
         print("Error: The folder specified in config.json doesn't exist.")
         sys.exit(1)
     except PermissionError:
-        print("Error: You don't have acces to the folder specified in config.json. Try running as administrator")
+        print("Error: You don't have access to the folder specified in config.json. Try running as administrator")
         sys.exit(1)
-
-
-def bullet_list(added, removed, updated):
-    markdown_text = ""
-
-    if added:
-        markdown_text += "## Added\n"
-        for mod in added:
-            markdown_text += f"- {mod}\n"
-
-    if removed:
-        markdown_text += "## Removed\n"
-        for mod in removed:
-            markdown_text += f"- {mod}\n"
-
-    if updated:
-        markdown_text += "## Updated\n"
-        for mod in updated:
-            markdown_text += f"- {mod}\n"
-
-    return markdown_text
-
-def comma_list(added, removed, updated):
-    markdown_text = ""
-
-    if added:
-        markdown_text += "* Added: "
-        for mod in added:
-            markdown_text += f"{mod}, "
-
-    if removed:
-        markdown_text += "\n* Removed: "
-        for mod in removed:
-            markdown_text += f"{mod}, "
-
-    if updated:
-        markdown_text += "\n* Updated: "
-        for mod in updated:
-            markdown_text += f"{mod}, "
-
-    return markdown_text
-
-def ind_bullet_list(added, removed, updated):
-    markdown_text = ""
-    markdown_text += "Changes: "
-    if added:
-        for mod in added:
-            markdown_text += f"\n* added {mod}"
-
-    if removed:
-        for mod in removed:
-            markdown_text += f"\n* removed {mod}"
-
-    if updated:
-        for mod in updated:
-            markdown_text += f"\n* updated {mod}"
-
-    return markdown_text
-
-def ind_comma_list(added, removed, updated):
-    markdown_text = ""
-    markdown_text += "Changes: "
-    if added:
-        for mod in added:
-            markdown_text += f"added {mod}, "
-
-    if removed:
-        for mod in removed:
-            markdown_text += f"removed {mod}, "
-
-    if updated:
-        for mod in updated:
-            markdown_text += f"updated {mod}, "
-
-    return markdown_text
