cd19c34c219fe10bcdd06844710242e4b045f88b
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-16 11:16:25 +0100

    refactor get_json.py

--- get_json.py
+++ get_json.py
@@ -1,24 +1,31 @@
 import os
 import json
 import sys
+import shutil
 from zipfile import ZipFile
 
 def get_json(path):
+    # Ensure the file is a .mrpack file
+    if not path.endswith('.mrpack'):
+        print('ERROR: Input file is not a .mrpack')
+        sys.exit(1)
+
     # Create a temporary directory
     temp_dir = os.path.join(os.environ.get('TEMP'), 'mrpack_Changelogger')
     os.makedirs(temp_dir, exist_ok=True)
 
-    # unpack the .mrpack file into the temp directory
-    if not path.endswith('.mrpack'):
-        print('ERROR: Input file is not a .mrpack')
-        sys.exit(1)
-    with ZipFile(path, 'r') as zObject:
-        zObject.extractall(path=temp_dir)
-
-    # parse the json file
     try:
-        with open(os.path.join(temp_dir, 'modrinth.index.json'), 'r', encoding="utf-8") as f:
-            return json.load(f)
+        # Unpack the .mrpack file into the temp directory
+        with ZipFile(path, 'r') as zip_obj:
+            zip_obj.extractall(path=temp_dir)
+
+        # Parse the json file
+        json_path = os.path.join(temp_dir, 'modrinth.index.json')
+        with open(json_path, 'r', encoding="utf-8") as json_file:
+            return json.load(json_file)
     except ValueError:
-        print('ERROR: one modrinth.index.json is not formatted correctly')
+        print(f'ERROR: The file {json_path} is not formatted correctly')
         sys.exit(1)
+    finally:
+        # Delete the extracted files
+        shutil.rmtree(temp_dir)
