176b74d98fb72c87551a51a239b32d087bc2cab7
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-16 10:41:40 +0100

    refactor error handling

--- Main.py
+++ Main.py
@@ -5,24 +5,7 @@ from out import markdown_out
 from get_json import get_json
 from config_handler import load_config, create_config
 
-def main():
-    if sys.argv[1] == "-cc" or sys.argv[1] == "--create-config":
-        create_config()
-        sys.exit(0)
-    elif (sys.argv[1] == "-o" or sys.argv[1] == "--old") and (sys.argv[3] == "-n" or sys.argv[3] == "--new"):
-        old_path = sys.argv[2]
-        new_path = sys.argv[4]
-    elif sys.argv[1] == "-h" or sys.argv[1] == "--help" or sys.argv[1] == "-?":
-        print("Usage:")
-        print("  -o, --old                   The pack to compare to")
-        print("  -n, --new                   The pack to compare")
-        print("  -cc, --create-config        Create a config file")
-        print("  -h, --help                  show this help message and exit")
-        sys.exit(0)
-    else:
-        print("Invalid arguments, use -h for help")
-        sys.exit(1)
-
+def main(old_path, new_path):
     config = load_config()
 
     # Parse the json files
@@ -36,4 +19,15 @@ def main():
     markdown_out(added, removed, updated, config)
 
 if __name__ == "__main__":
-    main()
+    if sys.argv[1] == "-cc" or sys.argv[1] == "--create-config":
+        create_config()
+    elif (sys.argv[1] == "-o" or sys.argv[1] == "--old") and (sys.argv[3] == "-n" or sys.argv[3] == "--new"):
+        main(sys.argv[2], sys.argv[4])
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help" or sys.argv[1] == "-?":
+        print("Usage:")
+        print("  -o, --old                   The pack to compare to")
+        print("  -n, --new                   The pack to compare")
+        print("  -cc, --create-config        Create a config file")
+        print("  -h, --help                  show this help message and exit")
+    else:
+        print("Error: Invalid arguments, use -h for help")
