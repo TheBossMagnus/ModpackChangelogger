d5e95b41a3efb7665865f515880119f9970a10c4
TheBossMagnus <TheBossMagnus@proton.me>
2023-11-18 11:17:24 +0100

    many fix to logging

--- Main.py
+++ Main.py
@@ -8,7 +8,22 @@ from config_handler import load_config, create_config
 
 # Initialize the logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+import logging
+
+# Set up the root logger
+def setup_logging(debug):
+    # Set up a file handler with a lower level (e.g., DEBUG)
+    file_handler = logging.FileHandler('log.txt')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+    # Set up a console handler with a higher level (e.g., INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter('%(message)s'))
+
+    # Add the handlers to the root logger
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO, handlers=[file_handler, console_handler])
 
 def main(old_path, new_path):
     config = load_config()
@@ -31,29 +46,15 @@ def handle_arguments():
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug logging")
     args = parser.parse_args()
 
-    # Set up a console handler with a higher level (e.g., INFO)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_formatter = logging.Formatter('%(message)s')
-    console_handler.setFormatter(console_formatter)
-    logger.addHandler(console_handler)
-
-    # Set up a file handler with a lower level (e.g., DEBUG)
-    file_handler = logging.FileHandler('log.txt')
-    file_handler.setLevel(logging.DEBUG if args.debug else logging.INFO)
-    file_handler.setFormatter(log_formatter)
-    logger.addHandler(file_handler)
+    setup_logging(args.debug)
 
     if args.debug:
         logger.info("Debug logging enabled")
 
     if args.create_config:
-        logger.debug("Created config.json")
         create_config()
     if args.old and args.new:
         main(args.old, args.new)
 
 if __name__ == "__main__":
-    logger.info("Starting")
     handle_arguments()
