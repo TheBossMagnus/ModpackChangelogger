c1e11ebb845080bfadfb1e8c45bfb323089916a0
TheBossMagnus <TheBossMagnus@proton.me>
2023-12-22 11:11:01 +0100

    performance improvments

--- get_mod_name.py
+++ get_mod_name.py
@@ -4,17 +4,16 @@ import aiohttp
 BASE_URL = "https://api.modrinth.com/v2/project"
 HEADERS = {'User-Agent':"TheBossMagnus/Mrpack_changelogger (thebossmagnus@proton.me)"}
 
-async def get_mod_name(mod_id):
+async def get_mod_name(session, mod_id):
     url = f"{BASE_URL}/{mod_id}"
 
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=HEADERS, timeout=10) as response:
-                response.raise_for_status()
-                response_json = await response.json()
-                mod_name = response_json.get('title')
-                logging.debug("Successfully retrieved name %s for ID %s", mod_name, mod_id)
-                return mod_name
+        async with session.get(url, headers=HEADERS, timeout=10) as response:
+            response.raise_for_status()
+            response_json = await response.json()
+            mod_name = response_json.get('title')
+            logging.debug("Successfully retrieved name %s for ID %s", mod_name, mod_id)
+            return mod_name
     except aiohttp.ClientResponseError as err:
         logging.warning("WARNING: Unable to retrieve project information for ID %s from Modrinth API. Received response code: %s. If the issue persists, consider reporting it on GitHub repository.", mod_id, err.status)
     except aiohttp.ClientConnectionError:
