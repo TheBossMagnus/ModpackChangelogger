7e7d21474fc0954990320e8b3ed2f4854d50ee20
TheBossMagnus <TheBossMagnus@proton.me>
2024-01-19 12:36:38 +0100

    Remove useless code
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/TMP.py
+++
@@ -1,2 +0,0 @@
-from ModpackChangelogger import main
-main(r"D:\ModpackChangelogger\tests\old.mrpack",r"D:\ModpackChangelogger\tests\new.mrpack", None, r"D:\ModpackChangelogger\tests\Changelog.md", True)
