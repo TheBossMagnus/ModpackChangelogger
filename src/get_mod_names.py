5a0878bd062d12e7cbc7ee4a74e8d01df431cf70
TheBossMagnus <TheBossMagnus@proton.me>
2024-01-23 21:20:57 +0100

    incrase web request timeout to 15
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/get_mod_names.py
+++ src/get_mod_names.py
@@ -5,7 +5,7 @@ import json
 from constants import MODRINTH_API_URL, HEADERS
 
 async def get_mod_names(added_ids, removed_ids, updated_ids):
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=15)) as session:
         added_names, removed_names, updated_names = await asyncio.gather(
             request_from_api(session, added_ids),
             request_from_api(session, removed_ids),
@@ -17,13 +17,13 @@ async def request_from_api(session, ids):
     # Convert the set of ids to a json array
     ids_list = json.dumps(list(ids))
     URL = f"{MODRINTH_API_URL}/projects?ids={ids_list}"
-    titles = []
+    names = []
 
     try:
         async with session.get(URL, headers=HEADERS) as response:
             response.raise_for_status()
             data = await response.json()
-            titles = [project.get('title') for project in data]
+            names = [project.get('title') for project in data]
     except aiohttp.ClientConnectionError as e:
         logging.warning("Failed to connect to %s: %s", URL, e)
     except asyncio.TimeoutError:
@@ -35,4 +35,4 @@ async def request_from_api(session, ids):
     except aiohttp.ClientError as e:
         logging.warning("An unexpected error occurred: %s", e)
 
-    return titles
+    return names
