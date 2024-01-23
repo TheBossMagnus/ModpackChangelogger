ec030da75c5477f40c3017ba9edcd40b3100fcaf
TheBossMagnus <TheBossMagnus@proton.me>
2024-01-23 21:17:49 +0100

    improve web requests
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/get_mod_names.py
+++ src/get_mod_names.py
@@ -1,25 +1,29 @@
-import logging
-import asyncio
 import aiohttp
+import asyncio
+import logging
+import json
 from constants import MODRINTH_API_URL, HEADERS
 
 async def get_mod_names(added_ids, removed_ids, updated_ids):
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
-        # Merge the list of ids and run the web request
-        tasks = [(request_from_api(session, id_), i) for i, ids in enumerate((added_ids, removed_ids, updated_ids)) for id_ in ids]
-        results = await asyncio.gather(*(task for task, _ in tasks))
-        # Split the list back
-        added_names = [result for result, index in zip(results, (index for _, index in tasks)) if index == 0]
-        removed_names = [result for result, index in zip(results, (index for _, index in tasks)) if index == 1]
-        updated_names = [result for result, index in zip(results, (index for _, index in tasks)) if index == 2]
+        added_names, removed_names, updated_names = await asyncio.gather(
+            request_from_api(session, added_ids),
+            request_from_api(session, removed_ids),
+            request_from_api(session, updated_ids)
+        )
         return added_names, removed_names, updated_names
 
-async def request_from_api(session, id_):
-    URL = f"{MODRINTH_API_URL}/project/{id_}"
+async def request_from_api(session, ids):
+    # Convert the set of ids to a json array
+    ids_list = json.dumps(list(ids))
+    URL = f"{MODRINTH_API_URL}/projects?ids={ids_list}"
+    titles = []
+
     try:
         async with session.get(URL, headers=HEADERS) as response:
             response.raise_for_status()
             data = await response.json()
+            titles = [project.get('title') for project in data]
     except aiohttp.ClientConnectionError as e:
         logging.warning("Failed to connect to %s: %s", URL, e)
     except asyncio.TimeoutError:
@@ -30,5 +34,5 @@ async def request_from_api(session, id_):
         logging.warning("Failed to read response from %s: %s", URL, e)
     except aiohttp.ClientError as e:
         logging.warning("An unexpected error occurred: %s", e)
-    else:
-        return data.get('title')
+
+    return titles
