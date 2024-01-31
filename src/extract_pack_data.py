044e39016d0bc2167e790dc0a1816f51cade2164
TheBossMagnus <TheBossMagnus@proton.me>
2024-01-31 18:33:48 +0100

    basic cf modpack parsing support
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/extract_pack_data.py
+++ src/extract_pack_data.py
@@ -29,6 +29,37 @@ def mr_get_pack_data(old_json, new_json):
     return old_ids, new_ids, old_info, new_info
 
 def cf_get_pack_data(old_json, new_json):
-    print("Not yet implemented")
+    print("Curseforge support Not yet fully implemented")
+
+    def get_dependency_info(json):
+        loader_string = json['minecraft']['modLoaders'][0]['id']
+        return {
+            'mc_version': json['minecraft']['version'],
+            'loader': loader_string.split('-')[0],
+            'loader_version': loader_string.split('-')[1]
+        }
+    
+
+    def analyze_json_files(old_json, new_json):
+
+        old_file_ids = {item['fileID']: item['projectID'] for item in old_json['files']}
+        new_file_ids = {item['fileID']: item['projectID'] for item in new_json['files']}
+
+        unique_old_project_ids = set()
+        unique_new_project_ids = set()
+
+        for file_id, project_id in old_file_ids.items():
+            if file_id not in new_file_ids:
+                unique_old_project_ids.add(project_id)
+
+        for file_id, project_id in new_file_ids.items():
+            if file_id not in old_file_ids:
+                unique_new_project_ids.add(project_id)
+
+        return unique_old_project_ids, unique_new_project_ids
+    
+    old_info, new_info = get_dependency_info(old_json), get_dependency_info(new_json)
+    old_ids, new_ids = analyze_json_files(old_json, new_json)
+    print(old_ids, new_ids)
     sys.exit(0)
-    return None, None, None, None
+    return old_ids, new_ids, old_info, new_info
