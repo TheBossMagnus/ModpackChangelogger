efb80c1bbed8b4cf990bd00b1943ca33d156255b
TheBossMagnus <TheBossMagnus@proton.me>
2024-02-21 19:46:32 +0100

    Vastly improved incomplete/wrong configs handling
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/config_handler.py
+++ src/config_handler.py
@@ -13,6 +13,19 @@ def create_config():
         logging.error("ERROR: Unable to create the config.json in %s. Try running as administrator", os.getcwd())
         sys.exit(1)
 
+def validate_config(config, default_config=DEFAULT_CONFIG):
+    """Check if all the fields are present and are of the correct type"""
+    if not isinstance(config, dict):
+        raise ValueError("Config must be a dictionary")
+    for key, default_value in default_config.items():
+        if key not in config:
+            config[key] = default_value
+        elif isinstance(default_value, dict): # Recursively validate nested keys
+            config[key] = validate_config(config.get(key, {}), default_value)
+        elif not isinstance(config[key], type(default_value)):
+            raise ValueError(f"Config field \"{key}\" must be of type {type(default_value).__name__}")
+    return config
+
 
 def load_config(config_file):
     if not config_file:
@@ -30,8 +43,13 @@ def load_config(config_file):
     try:
         with open(config_file, 'r', encoding="utf-8") as f:
             config = json.load(f)
-        logging.debug('Loaded config from %s', config_file)
-        return config
-    except ValueError:
-        logging.error("ERROR: %s is not formatted correctly", config_file)
+            config = validate_config(config, DEFAULT_CONFIG)
+        logging.debug('CONFIG: %s', config)
+    except json.JSONDecodeError:
+        logging.error("ERROR: %s is not a valid JSON file", config_file)
         sys.exit(1)
+    except ValueError as e:
+        logging.error("ERROR: %s", e)
+        sys.exit(1)
+
+    return config
