cb91ffb21bd49c3324f16d0d83e4921cfae0d57d
TheBossMagnus <TheBossMagnus@proton.me>
2024-04-28 21:15:45 +0200

    Fix some argparse issues and update --help infos
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- modpack_changelogger/cli_wrapper.py
+++ modpack_changelogger/cli_wrapper.py
@@ -10,11 +10,14 @@ def wrapper():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--old", help="First pack to compare")
     parser.add_argument("-n", "--new", help="The pack to compare against")
-    parser.add_argument("-c", "--config", default=None, nargs="?", const="new", help="Use a config file; 'new' creates a new one")
-    parser.add_argument("-f", "--file", default=None, help="Specify the output file for the changelog, 'console' prints to console")
+    parser.add_argument("-c", "--config", help="Use a config file")
+    parser.add_argument("-f", "--file", help="Specify the output file for the changelog")
     parser.add_argument("-v", "--version", action="store_true", help="Print the version number")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug logging")
     args = parser.parse_args()
     if args.version:
         print(f"ModpackChangelogger {VERSION}")
+        if not (args.old or args.new or args.config or args.file):  # If the user only wants the version number
+            return
+
     generate_changelog(args.old, args.new, args.config, args.file, args.debug)
