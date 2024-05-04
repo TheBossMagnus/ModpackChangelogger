12bf5fa51c20a3cf126f5f195fecb6c4d3383f42
TheBossMagnus <TheBossMagnus@proton.me>
2024-05-04 14:52:13 +0200

    refactor test unit
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- test/test.py
+++ test/test.py
@@ -3,17 +3,19 @@ import os
 import subprocess
 import sys
 import unittest
+import unittest.mock
 
 from modpack_changelogger.main import generate_changelog
 
 
-class BasicTest(unittest.TestCase):
+class Test(unittest.TestCase):
+
     def test_mr_packs(self):
         old_pack = "test/packs/old.mrpack"
         new_pack = "test/packs/new.mrpack"
         expected_output = "test/expected/t1.md"
 
-        generate_changelog(old_pack, new_pack, None, None, False)
+        generate_changelog(old_pack, new_pack, None, None)
         self.assertTrue(filecmp.cmp("Changelog.md", expected_output, shallow=False))
 
     def test_cf_packs(self):
@@ -21,7 +23,7 @@ class BasicTest(unittest.TestCase):
         new_pack = "test/packs/new.zip"
         expected_output = "test/expected/t2.md"
 
-        generate_changelog(old_pack, new_pack, None, None, False)
+        generate_changelog(old_pack, new_pack, None, None)
         self.assertTrue(filecmp.cmp("Changelog.md", expected_output, shallow=False))
 
     def test_config_parameters(self):
@@ -31,6 +33,21 @@ class BasicTest(unittest.TestCase):
         expected_output = "test/expected/t3.md"
         generate_changelog(old_pack, new_pack, config_path, None)
         self.assertTrue(filecmp.cmp("Changelog.md", expected_output, shallow=False))
+
+    def test_config_parameters_2(self):
+        old_pack = "test/packs/old.mrpack"
+        new_pack = "test/packs/new.mrpack"
+        config_path = "test/configs/config2.json"
+        expected_output = "test/expected/t4.md"
+        generate_changelog(old_pack, new_pack, config_path, None)
+        self.assertTrue(filecmp.cmp("Changelog.md", expected_output, shallow=False))
+
+    def test_config_parameters_3(self):
+        old_pack = "test/packs/old.mrpack"
+        new_pack = "test/packs/new.mrpack"
+        config_path = "test/configs/config3.json"
+        with self.assertRaises(SystemExit):
+            generate_changelog(old_pack, new_pack, config_path, None, False)
 
     def test_run_as_script(self):
         script_path = r"modpack_changelogger.py"
@@ -48,24 +65,6 @@ class BasicTest(unittest.TestCase):
         for file in ["Changelog.md", "config.json", "log.txt", "name.md"]:
             if os.path.exists(file):
                 os.remove(file)
-
-
-class FullTests(BasicTest):
-
-    def test_config_parameters_2(self):
-        old_pack = "test/packs/old.mrpack"
-        new_pack = "test/packs/new.mrpack"
-        config_path = "test/configs/config2.json"
-        expected_output = "test/expected/t4.md"
-        generate_changelog(old_pack, new_pack, config_path, None)
-        self.assertTrue(filecmp.cmp("Changelog.md", expected_output, shallow=False))
-
-    def test_config_parameters_3(self):
-        old_pack = "test/packs/old.mrpack"
-        new_pack = "test/packs/new.mrpack"
-        config_path = "test/configs/config3.json"
-        with self.assertRaises(SystemExit):
-            generate_changelog(old_pack, new_pack, config_path, None, False)
 
 
 if __name__ == "__main__":
