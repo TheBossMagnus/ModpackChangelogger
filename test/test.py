6255c8c5b16979d064d258041605bcb15dc675d7
TheBossMagnus <TheBossMagnus@proton.me>
2024-05-06 21:44:40 +0200

    Reimplement part of the test unit
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- test/test.py
+++ test/test.py
@@ -9,67 +9,41 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) 
 
 from modpack_changelogger.main import generate_changelog
 
+old_pack = "test/packs/old.mrpack"
+new_pack = "test/packs/new.mrpack"
 
-def test_mr_packs():
-    old_pack = "test/packs/old.mrpack"
-    new_pack = "test/packs/new.mrpack"
-    expected_output = "test/expected/t1.md"
+
+def test_mr():
+    expected_output = "test/expected/mr.md"
 
     generate_changelog(old_pack, new_pack, None, None)
     assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
 
 
-def test_cf_packs():
+def test_cf():
     old_pack = "test/packs/old.zip"
     new_pack = "test/packs/new.zip"
-    expected_output = "test/expected/t2.md"
+    expected_output = "test/expected/cf.md"
 
     generate_changelog(old_pack, new_pack, None, None)
     assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
 
-
-def test_config_parameters():
-    old_pack = "test/packs/old.mrpack"
-    new_pack = "test/packs/new.mrpack"
-    config_path = "test/configs/config1.json"
-    expected_output = "test/expected/t3.md"
-
-    generate_changelog(old_pack, new_pack, config_path, None)
-    assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
-
-
-def test_config_parameters_2():
-    old_pack = "test/packs/old.mrpack"
-    new_pack = "test/packs/new.mrpack"
-    config_path = "test/configs/config2.json"
-    expected_output = "test/expected/t4.md"
-
-    generate_changelog(old_pack, new_pack, config_path, None)
-    assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
-
-
-def test_config_parameters_3():
-    old_pack = "test/packs/old.mrpack"
-    new_pack = "test/packs/new.mrpack"
-    config_path = "test/configs/config3.json"
+def test_broken_config():
+    config_path = "test/configs/broken_config.json"
 
     with pytest.raises(SystemExit):
         generate_changelog(old_pack, new_pack, config_path, None, False)
 
 
 def test_run_as_script():
-    script_path = r"modpack_changelogger.py"
-    old_pack = "test/packs/old.mrpack"
-    new_pack = "test/packs/new.mrpack"
-    expected_output = "test/expected/t5.md"
+    script_name = "modpack_changelogger.py"
+    expected_output = "test/expected/run_as_script.md"
 
-    result = subprocess.run([sys.executable, script_path, "-o", old_pack, "-n", new_pack, "-f", "name.md", "-c", "new", "-d"], check=False)
+    result = subprocess.run([sys.executable, script_name, "-o", old_pack, "-n", new_pack], check=False)
 
     assert result.returncode == 0
-    assert filecmp.cmp("name.md", expected_output, shallow=False)
-    assert os.path.exists("config.json")
-    assert os.path.exists("log.txt")
+    assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
 
-    for file in ["Changelog.md", "config.json", "log.txt", "name.md"]:
-        if os.path.exists(file):
-            os.remove(file)
+    
+    if os.path.exists("Changelog.md"):
+        os.remove("Changelog.md")
