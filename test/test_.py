6194ab7037a2bb4dba6568882a4dec1a9c08a997
TheBossMagnus <TheBossMagnus@proton.me>
2024-05-09 21:57:42 +0200

    add 3 new test and lints
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- test/test_.py
+++ test/test_.py
@@ -1,54 +1,89 @@
 import filecmp
+import io
 import os
 import subprocess
 import sys
 
 import pytest
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # noqa # pylint disable=wrong-import-position
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from modpack_changelogger.main import generate_changelog
+from modpack_changelogger.main import generate_changelog  # pylint:disable=C0413
 
-old_pack = "test/packs/old.mrpack"
-new_pack = "test/packs/new.mrpack"
+OLD_PACK = "test/packs/old.mrpack"
+NEW_PACK = "test/packs/new.mrpack"
 
 
 def test_mr():
     expected_output = "test/expected/mr.md"
 
-    generate_changelog(old_pack, new_pack, None, None)
+    generate_changelog(OLD_PACK, NEW_PACK, None, None)
     assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
 
 
 def test_cf():
-    old_pack = "test/packs/old.zip"
-    new_pack = "test/packs/new.zip"
     expected_output = "test/expected/cf.md"
 
-    generate_changelog(old_pack, new_pack, None, None)
+    generate_changelog("test/packs/old.zip", "test/packs/new.zip", None, None)
     assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
 
 
 def test_formatted():
     expected_output = "test/expected/formatted.md"
-    with open(expected_output, "r") as file:
+    with open(expected_output, "r", encoding="utf-8") as file:
         expected_output = file.read()
 
-    assert expected_output == generate_changelog(old_pack, new_pack, None, "formatted")
+    assert expected_output == generate_changelog(OLD_PACK, NEW_PACK, None, "formatted")
+
+
+def test_unformatted():
+    expected_output_file = "test/expected/unformatted.md"
+
+    output1, output2, output3 = generate_changelog(OLD_PACK, NEW_PACK, None, "unformatted")
+
+    with open(expected_output_file, "r", encoding="utf-8") as f:
+        expected1, expected2, expected3 = f.read().split("\n")
+
+    assert str(output1) == expected1.strip()
+    assert str(output2) == expected2.strip()
+    assert str(output3) == expected3.strip()
+
+
+def test_console():
+    expected_output_file = "test/expected/console.md"
+
+    console_output = io.StringIO()
+    sys.stdout = console_output
+    generate_changelog(OLD_PACK, NEW_PACK, None, "console")
+    sys.stdout = sys.__stdout__
+    console_output = console_output.getvalue()
+
+    with open(expected_output_file, "r", encoding="utf-8") as file:
+        expected_output = file.read()
+
+    assert expected_output == console_output
+
+
+def test_config_new():
+    generate_changelog(None, None, "new", None)
+    try:
+        assert os.path.isfile("config.json")
+    finally:
+        os.remove("config.json")
 
 
 def test_broken_config():
     config_path = "test/configs/broken_config.json"
 
     with pytest.raises(SystemExit):
-        generate_changelog(old_pack, new_pack, config_path, None)
+        generate_changelog(OLD_PACK, NEW_PACK, config_path, None)
 
 
 def test_run_as_script():
     script_name = "modpack_changelogger.py"
     expected_output = "test/expected/run_as_script.md"
 
-    result = subprocess.run([sys.executable, script_name, "-o", old_pack, "-n", new_pack], check=False)
+    result = subprocess.run([sys.executable, script_name, "-o", OLD_PACK, "-n", NEW_PACK], check=False)
 
     assert result.returncode == 0
     assert filecmp.cmp("Changelog.md", expected_output, shallow=False)
