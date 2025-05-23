aed5d73a7ab067988cc0699dbc94fa71f0267f6a
TheBossMagnus <TheBossMagnus@proton.me>
2025-05-23 18:20:04 +0200

    fix: exe not working

--- modpack_changelogger/__main__.py
+++ modpack_changelogger/__main__.py
@@ -5,7 +5,7 @@ Main entry point for running Modpack Changelogger as a module.
 This allows the package to be imported in other Python scripts.
 """
 
-from .cli_wrapper import main
+from modpack_changelogger.cli_wrapper import main
 
 if __name__ == "__main__":
     main()
